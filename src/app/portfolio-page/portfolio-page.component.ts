a8037f8bf691a518bd493d10cf7b2a1490b746b3
elena <vorst.hel@gmail.com>
2025-10-02 20:51:15 -0700

    search tags

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, injec
 import {CommonModule} from "@angular/common";
 import {S3Service} from "../services/s3.service";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
-import {BehaviorSubject, shareReplay, tap} from "rxjs";
+import {BehaviorSubject, map, shareReplay, switchMap, tap} from "rxjs";
 import {MatButtonModule} from "@angular/material/button";
 import {ViewImage} from "../dialogs/view-image/view-image";
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
@@ -13,6 +13,7 @@ import {Contactdialog} from "../dialogs/contactdialog/contactdialog";
 const DEFAIULT_SHOWN_IMAGES_INDEX=9;
 const DEFAIULT_SHOWN_IMAGES_INDEX_MOBILE=5;
 const DEFAIULT_LOAD_MORE_SIZE=12;
+const DELIMITER=' ₓ ';
 
 @Component({
   selector: 'app-portfolio-page',
@@ -25,6 +26,7 @@ const DEFAIULT_LOAD_MORE_SIZE=12;
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PortfolioPageComponent {
+  DELIMITER = DELIMITER;
   private readonly destroyRef = inject(DestroyRef);
   private readonly s3Service = inject(S3Service);
   private readonly cdr = inject(ChangeDetectorRef);
@@ -38,12 +40,41 @@ export class PortfolioPageComponent {
 
   readonly DEFAIULT_SHOWN_IMAGES_INDEX = this.isMobile ? DEFAIULT_SHOWN_IMAGES_INDEX_MOBILE : DEFAIULT_SHOWN_IMAGES_INDEX;
 
-  readonly folderMetas$ = this.s3Service.listFoldersWithMetadata()
+  private readonly foldersMetasOriginal$ = this.s3Service.listFoldersWithMetadata();
+
+  readonly tagFilter$ = new BehaviorSubject<string>('');
+
+  readonly folderMetas$ = this.foldersMetasOriginal$
     .pipe(
+      switchMap(folders => {
+        return this.tagFilter$.pipe(
+          map(tagFilter => {
+            if(!tagFilter) return folders;
+
+            return folders.filter(folder => folder.metadata.keywords.includes(tagFilter))
+          }),
+        )
+      }),
       tap(() => this.loading$.next(false)),
       takeUntilDestroyed(this.destroyRef),
       shareReplay({refCount: true, bufferSize: 1})
     );
+
+  readonly tags$ = this.foldersMetasOriginal$.pipe(
+    map(metas => metas.map(meta => meta.metadata.keywords).flat()),
+    map(tags => {
+      const m = new Map<string, number>();
+      for (const tag of tags) {
+        const tagLowercase = tag.toLowerCase();
+        let existing = m.get(tagLowercase) || 0;
+        existing++;
+        m.set(`${tagLowercase}`, existing);
+      }
+      return Array.from(m.entries()).sort((a, b) => b[1] - a[1]).map(e => `${e[0]}${DELIMITER}${e[1]}`).slice(0, 10);
+    }),
+    takeUntilDestroyed(this.destroyRef),
+    shareReplay({refCount: true, bufferSize: 1})
+  )
 
   showMore(folderPath: string) {
     const current = this.mapFolderKeyToShownImageIndex.get(folderPath) || DEFAIULT_SHOWN_IMAGES_INDEX;
@@ -83,5 +114,11 @@ export class PortfolioPageComponent {
     const portal = new ComponentPortal(Contactdialog);
     const compRef = this.overlayRef.attach(portal);
     compRef.instance.close.subscribe(() => this.overlayRef?.dispose());
+  }
+
+  setFilterKeyword(tag: string) {
+    const tagLowercase = tag.split(DELIMITER)[0];
+    const next = this.tagFilter$.getValue() === tagLowercase ? '' : tagLowercase;
+    this.tagFilter$.next(next);
   }
 }
